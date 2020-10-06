181c96be0638bdb1ff99317b03ee1cd2aba50673
KlimukVI <KlimukVI@rambler.ru>
2020-10-06 23:05:18 +0300

    refactor: remake using built-in methods

--- src/index.js
+++ src/index.js
@@ -3,14 +3,5 @@
 
 module.exports = function towelSort (matrix) {
   if (arguments.length === 0 || matrix.length === 0) {return [];}
-  let i = 0, matrixSort = [];
-  while (i < matrix.length) {
-    if (i % 2 === 0) {
-      matrixSort = matrixSort.concat(matrix[i])
-    } else {
-      matrixSort = matrixSort.concat(matrix[i].reverse())
-    }
-    i++;
-  }
-  return matrixSort;
+  return matrix.reduce((sum, current, index) => index % 2 === 0 ? sum.concat(current) : sum.concat(current.reverse()) );
 }
